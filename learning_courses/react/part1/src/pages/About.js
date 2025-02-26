50f475063c725cb9a839900699e61e905c318ea5
Sabri VAN DE VELDE <svan-de-@bess-f4r1s10.clusters.42paris.fr>
2025-02-26 22:22:55 +0100

    react part1/2 finished

---
+++ learning_courses/react/part1/src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere harum optio mollitia pariatur excepturi laborum quasi ex molestiae, cumque animi quaerat, illum magnam provident quisquam deleniti modi, impedit quam est! Beatae excepturi eaque inventore tenetur quam, placeat quia fuga ducimus tempore maiores nulla voluptates dicta sapiente. Nisi praesentium reprehenderit quae. Ullam, ab blanditiis! Suscipit nemo, unde eligendi autem dolores qui officiis nostrum, similique maiores molestiae at incidunt amet sint. Sed esse dolorem vero mollitia. Officia iusto vitae quisquam amet, quam ab libero veniam eaque, accusamus molestiae ratione ea quidem enim fugiat, blanditiis numquam. Repudiandae nisi harum saepe a magnam maxime.</p>
+            <br />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet maxime nam voluptatibus nulla tenetur doloribus architecto labore sunt quo, quis iure explicabo dolore reiciendis fugit distinctio? Sequi magni maiores laborum alias unde aliquid totam iure, molestias similique dicta vitae quo deleniti laboriosam nemo ipsam hic animi quod sed porro facilis magnam quas temporibus cum? Illo veritatis molestiae velit magni excepturi ipsam, esse culpa minus laboriosam inventore! Dicta reprehenderit enim nisi esse! Eaque fugit alias dolor, eius a libero sed accusantium placeat, iusto laborum ea? Consectetur ullam neque consequuntur vitae harum quaerat perferendis, repellat hic, eveniet magnam dicta aliquid veritatis facilis.</p>
+        </div>
+    );
+};
+
+export default About;
