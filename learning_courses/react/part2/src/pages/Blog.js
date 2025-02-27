0a7546f5cad7907ebfd3b1e769b360474c051fab
Sabri VAN DE VELDE <svan-de-@bess-f4r1s10.clusters.42paris.fr>
2025-02-27 04:15:52 +0100

    react part2 started

---
+++ learning_courses/react/part2/src/pages/Blog.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const Blog = () => {
+    const [content, setContent] = useState("");
+    const [error, setError] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (content.length < 140)
+            setError(true);
+        else
+            setError(false);
+    }
+
+    return (
+        <div className="blog-container">
+            <Logo />
+            <Navigation />
+            <h1>Blog</h1>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input type="text" placeholder='Nom' />
+                <textarea 
+                style={{border: error ? "1px solid red" : "1px solid #61dafb"}}
+                placeholder='Message' 
+                onChange={(e) => setContent(e.target.value)}>
+                </textarea>
+                {error && <p>Veuillez ecrire un minimum de 140 characteres</p>}
+                <input type="submit" value="Envoyer" />
+            </form>
+        </div>
+    );
+};
+
+export default Blog;
